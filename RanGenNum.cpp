f3a84502bcade429c123f5898ccc4ea082f0aaeb
Hani-hue <isnahabibah8729@gmail.com>
2025-04-22 15:21:02 +0700

    Random Number Generator (Limited)

--- RanGenNum.cpp
+++ RanGenNum.cpp
@@ -6,8 +6,13 @@ int main()
 
     srand(time(NULL));
 
-    int num = rand();
-    cout << num;
+    int num1 = (rand() % 15) + 1;
+    int num2 = (rand() % 15) + 1;
+    int num3 = (rand() % 15) + 1;
+
+    cout << num1 << '\n';
+    cout << num2 << '\n';
+    cout << num3 << '\n';
 
     return 0;
 }
