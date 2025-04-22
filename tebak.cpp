3bbf4f6f02e0b6a1fe119ccfcafe6d3241a280fe
Hani-hue <isnahabibah8729@gmail.com>
2025-04-22 23:24:26 +0700

    hoho (undone)

---
+++ tebak.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+string GenIm;
+string ZZZ;
+string MarRi;
+string WuWe;
+string LADS;
+char hehe;
+string starhon;
+
+int main()
+{
+    cout << "Choose your favourite game: \n";
+    return 0;
+}
